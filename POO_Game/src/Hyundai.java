5b67b0fb99239ad499de6f5b76e9bc9e854857d7
Axl <axl101100@hotmail.com>
2024-12-14 17:31:06 -0500

    Se agregó la clase Hyundai con un comentario

---
+++ POO_Game/src/Hyundai.java
@@ -0,0 +1,7 @@
+public class Hyundai {
+    // Clase creada por AXL BLADYMIR VASQUEZ
+    public static void main(String[] args) {
+        System.out.println("Clase Hyundai creada exitosamente.");
+    }
+
+}
